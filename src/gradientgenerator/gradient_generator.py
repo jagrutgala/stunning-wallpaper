98ee9edc9ef24a6ce7aa67408c513b67a55b4937
jagrutgala <jagrutgala@gmail.com>
2022-11-01 16:02:00 +0530

    Save image to data folder
    image gets a random name in pattern bg-XXXXX.png

--- src/gradientgenerator/gradient_generator.py
+++ src/gradientgenerator/gradient_generator.py
@@ -1,5 +1,6 @@
 from abc import ABC
 from typing import List, Tuple
+from random import randint
 from PIL import Image
 
 
@@ -21,16 +22,17 @@ def main():
     width = 1920
     height = 1080
     image_gardient = Image.new("RGB", (width, height))
-    color_list = [
-        (255, 0, 0),
-        (255, 255, 0),
-        (255, 255, 255),
-        (255, 0, 255),
-        (0, 255, 255)
-    ]
+    # color_list = [
+    #     (255, 0, 0),
+    #     (255, 255, 0),
+    #     (255, 255, 255),
+    #     (255, 0, 255),
+    #     (0, 255, 255)
+    # ]
+    color_list = [(randint(0,255), randint(0,255), randint(0,255)) for _ in range(5)]
     from gradientgenerator.simple_gardients import horizontal_graident, vertical_gradient
-    horizontal_graident(image_gardient, color_list).show()
-    vertical_gradient(image_gardient, color_list).show()
+    # horizontal_graident(image_gardient, color_list).show()
+    vertical_gradient(image_gardient, color_list).save(f"./data/bg-{''.join([str(randint(0,9)) for _ in range(5)])}.png")
 
 
 if __name__ == "__main__":
